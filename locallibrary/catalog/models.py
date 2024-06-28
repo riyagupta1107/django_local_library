44f15e6991f8f5326c52b91382b3bdf92e517452
riyagupta1107 <riya.gupta070705@gmail.com>
2024-06-28 20:21:25 +0530

    update models.py

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
 
+import uuid #required for unique book instances
+
 class Genre(models.Model):
     #Model representing a book genre
 
@@ -65,3 +67,84 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book"""
         return reverse('book-detail', args = [str(self.id)])
+    
+class BookInstance(models.Model):
+    """Model representing a specific copy of book (i.e. that can be borrowed from library)"""
+    
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        help_text = "Unique ID for the book"
+    )
+    book = models.ForeignKey(
+        'Book',
+        on_delete = models.RESTRICT,
+        null = True
+    )
+    imprint = models.CharField(max_length = 100)
+    due_back = models.DateField(null = True, blank = True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
+    
+class Author(models.Model):
+    """Model representing an author"""
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+    
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
+    
+class Language(models.Model):
+    """Model representing language"""
+
+    lang = models.CharField(
+        max_length = 100,
+        unique = True,
+        help_text = "Enter the language of the book"
+    )
+    def get_absolute_url(self):
+        """Returns the url to access a particular language instance."""
+        return reverse('language-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.lang
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                lang = 'language_name_case_insensitive_unique',
+                violation_error_message = "Language already exists"
+            ),
+        ]
