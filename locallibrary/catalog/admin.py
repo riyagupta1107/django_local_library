a199f80e59875238e53c20489fde2048dd8ad891
riyagupta1107 <riya.gupta070705@gmail.com>
2024-07-02 05:35:53 +0530

    updating admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,39 +2,24 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
 
-"""class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name','date_of_birth','date_of_death')
 
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
+admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-
-    inlines = [BooksInstanceInline]
+    pass
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
-
-    fieldsets = (
-        (None, {
-            'fields': ('book', 'imprint', 'id')
-        }),
-        ('Availability', {
-            'fields': ('status', 'due_back')
-        }),
-    )
-
-admin.site.register(Author, AuthorAdmin)"""
+    pass
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,2 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello, Django!")
